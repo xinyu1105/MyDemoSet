b6f5890024a5bf8248a2ed7038d95196faad512e
pengjiaxin <pengjiaxin@p-yf-ldf-151.tiancitycn.com>
2018-03-23 16:02:39 +0800

    Initial Commit

---
+++ MYDemoSet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYDemoSet
+//
+//  Created by pengjiaxin on 2018/3/23.
+//  Copyright © 2018年 pengjiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
